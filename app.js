9a39f99164e5aab6c57210c5083210978b565c1a
Parth Bhardwaj <parth@Parths-MacBook-Air.local>
2021-02-13 02:41:37 +0530

    minor add ons

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-/*Generate a random keyCode
-function randomKeyCode() {
-  return Math.floor(Math.random() * (90 - 65)) + 65;
-}
-*/
-/*show traget key
-function showRandomKey() {
-  let keycode = randomKeyCode();
-  let key = document.querySelector(`.key[data-key="${keycode}"]`);
-
-  key.classList.add("target");
-}
-*/
-/* main game mech
-
-window.addEventListener("keydown", function (e) {
-  let enteredKey = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  console.log(enteredKey);
-  let current = randomKeyCode();
-  showRandomKey();
-  let key = document.querySelector(`.key[data-key="${current}"]`);
-  console.log(key);
-  if (enteredKey == key) {
-    key.classList.remove("target");
-  }
-});
-*/
 var randKey = generateRandomKey();
 var score = document.querySelector(".score");
 var highScore = 0;
@@ -32,18 +5,19 @@ var highScoreDisp = document.querySelector(".hi-sc");
 
 window.addEventListener("keydown", function (e) {
   let enteredKey = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  console.log(enteredKey);
   let targetKeyCode = randKey;
   let targetKey = document.querySelector(`.key[data-key="${targetKeyCode}"]`);
-  console.log(targetKey);
+
+  /*comparing the entered key and the target*/
+
   if (enteredKey == targetKey) {
     targetKey.classList.remove("target");
     let nextKey = generateRandomKey();
     randKey = nextKey;
     score.innerText++;
-    console.log(score);
   } else {
     if (highScore < Number(score.innerText)) {
+      /*checking if current score is greater than high score*/
       highScore = score.innerText;
       highScoreDisp.innerText = highScore;
     }
@@ -51,6 +25,8 @@ window.addEventListener("keydown", function (e) {
     alert("Ahh !! Lost the streak :( \n Try again !!");
   }
 });
+
+/* Generate Random key */
 
 function generateRandomKey() {
   let keycode = Math.floor(Math.random() * (90 - 65)) + 65;
